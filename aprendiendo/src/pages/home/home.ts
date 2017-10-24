413b77fbb6b9c349b57d4d5d50e289c3c2c0c3d6
zedin1994 <94gagb@gmail.com>
2017-10-23 21:31:30 -0400

    Cambios en vista home

--- aprendiendo/src/pages/home/home.ts
+++ aprendiendo/src/pages/home/home.ts
@@ -8,11 +8,9 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 	public lineChartData:Array<any> = [
-    {data: [65, 59, 80, 81, 56, 55, 40], label: 'Series A'},
-    {data: [28, 48, 40, 19, 86, 27, 90], label: 'Series B'},
-    {data: [18, 48, 77, 9, 100, 27, 40], label: 'Series C'}
+    {data: [40,50, 60, 62, 66, 70, 50, 60, 70, 80, 90, 100], label: 'Peso'}
   ];
-  public lineChartLabels:Array<any> = ['January', 'February', 'March', 'April', 'May', 'June', 'July'];
+  public lineChartLabels:Array<any> = ['Sem 1', 'Sem 2', 'Sem 3', 'Sem 4'];
   public lineChartOptions:any = {
     responsive: true
   };
@@ -24,23 +22,8 @@ export class HomePage {
       pointBorderColor: '#fff',
       pointHoverBackgroundColor: '#fff',
       pointHoverBorderColor: 'rgba(148,159,177,0.8)'
-    },
-    { // dark grey
-      backgroundColor: 'rgba(77,83,96,0.2)',
-      borderColor: 'rgba(77,83,96,1)',
-      pointBackgroundColor: 'rgba(77,83,96,1)',
-      pointBorderColor: '#fff',
-      pointHoverBackgroundColor: '#fff',
-      pointHoverBorderColor: 'rgba(77,83,96,1)'
-    },
-    { // grey
-      backgroundColor: 'rgba(148,159,177,0.2)',
-      borderColor: 'rgba(148,159,177,1)',
-      pointBackgroundColor: 'rgba(148,159,177,1)',
-      pointBorderColor: '#fff',
-      pointHoverBackgroundColor: '#fff',
-      pointHoverBorderColor: 'rgba(148,159,177,0.8)'
     }
+    
   ];
   public lineChartLegend:boolean = true;
   public lineChartType:string = 'line';
